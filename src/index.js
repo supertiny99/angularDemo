88b0abf571139c03166205cabfd8aafefd76cf75
supertiny <supertiny99@126.com>
2016-07-21 13:53:19 +0800

    ng-switch on ng-cloak

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 angular.module('exampleApp', [])
   .controller('defaultCtrl', ($scope) => {
+
+    $scope.data = {};
+
     $scope.todos = [
       { action: '活动1', complete: false},
       { action: '活动2', complete: false},
