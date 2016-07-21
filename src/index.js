74ec983d4cf008610449b970b92c18a551ac263d
supertiny <supertiny99@126.com>
2016-07-21 16:11:36 +0800

    test ng-disable

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 angular.module('exampleApp', [])
-  .controller('defaultCtrl', ($scope, $location) => {
+  .controller('defaultCtrl', ($scope) => {
 
-    $scope.message = '轻击我！'
-  }).directive('tap', () => {
-    return (scope, elem, attrs) => {
-      elem.on('touchstart touchend', () => {
-        scope.$apply(attrs['tap']);
-      });
-    }
+    $scope.dataValue = false;
   });
