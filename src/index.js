460ba9e5a630c4fb0c7c2ed489f52cbf2af6e3ad
supertiny <supertiny99@126.com>
2016-07-21 14:04:10 +0800

    元素与事件指令 初始化

---
+++ src/index.js
@@ -0,0 +1,12 @@
+angular.module('exampleApp', [])
+  .controller('defaultCtrl', ($scope) => {
+
+    $scope.data = {};
+
+    $scope.todos = [
+      { action: '活动1', complete: false},
+      { action: '活动2', complete: false},
+      { action: '活动3', complete: false},
+      { action: '活动4', complete: true}
+    ];
+  });
